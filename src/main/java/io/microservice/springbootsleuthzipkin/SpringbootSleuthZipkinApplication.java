d1b6426c20930273b194627ddfc45d832b86455c
Priyobrato Das <priyodas12@gmail.com>
2020-08-31 22:17:30 +0530

    logger added

--- src/main/java/io/microservice/springbootsleuthzipkin/SpringbootSleuthZipkinApplication.java
+++ src/main/java/io/microservice/springbootsleuthzipkin/SpringbootSleuthZipkinApplication.java
@@ -2,8 +2,10 @@ package io.microservice.springbootsleuthzipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"io.microservice.springbootsleuthzipkin.*"})
 public class SpringbootSleuthZipkinApplication {
 
 	public static void main(String[] args) {
